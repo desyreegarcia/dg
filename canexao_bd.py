0da9829a71ea70be78360b136a6fde4e7c1faea9
Desyree Garcia <desyree.garcia@hotmail.com>
2025-04-06 00:37:25 -0300

    banco de dados ok cirrigir interface

--- canexao_bd.py
+++ canexao_bd.py
@@ -10,20 +10,20 @@ class BandoDeDados:
     def criar_tabelas(self):
         query_produto = """
             CREATE TABLE IF NOT EXISTS produto (
-                id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 categoria TEXT(255) NOT NULL,
                 tamanho TEXT(10) NOT NULL,
                 valor_venda REAL NOT NULL)
         """
         query_cliente = """
             CREATE TABLE IF NOT EXISTS cliente (
-                id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 nome TEXT(255) NOT NULL,
                 telefone TEXT(11) NOT NULL);
         """
         query_venda = """
             CREATE TABLE IF NOT EXISTS venda (
-                id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 cliente_id INTEGER NOT NULL,
                 produto_id INTEGER NOT NULL,
                 data_venda DATETIME NOT NULL,
@@ -73,7 +73,18 @@ class BandoDeDados:
             query = 'SELECT * FROM venda'
             return self.connection.execute(query).fetchall()
     
-banco = BandoDeDados("vendas_dg.db")
+banco = BandoDeDados("vendas_dgpijamas.db")
+
 #banco.criar_tabelas()
+
+'''
+json_produto = {'categoria':'testeJSon', 'tamanho':'1', 'valor_venda':'1'}
+banco.cadastrar_produto(json_produto)
+resultado = banco.selecionar_produto(1)
+print(resultado)'''
+
+'''
+json_cliente = {'nome':'testeJSon', 'telefone':'11999999999'}
+banco.cadastrar_cliente(json_cliente)
 resultado = banco.selecionar_cliente(1)
-print(resultado)
+print(resultado)'''
--- interface.py
+++ interface.py
@@ -1,30 +1,24 @@
 import tkinter as tk
 from tkinter import Tk, Label, Entry, Button, messagebox
 from datetime import datetime
+from canexao_bd import BandoDeDados
 from produto import Produto
 from cliente import Cliente
 
-# Conectar ao banco
-#conn, cursor = conexao_bd()
+conexao = BandoDeDados("vendas_dgpijamas.db")
 
 # Função para cadastrar produtos
 def cadastrar_produto():
-    categoria = entry_categoria.get()
-    tamanho = entry_tamanho.get()
-    valor = float(entry_valor.get())
-
-    #cursor.execute("INSERT INTO produtos (categoria, tamanho, valor_venda) VALUES (%s, %s, %s)", 
-                   #(categoria, tamanho, valor))
-    #conn.commit()
+    print(f'categoria {categoria}, tamanho {tamanho} e preço de venda {valor_venda}')
+    conexao.cadastrar_produto({'categoria':categoria, 'tamanho':tamanho, 'valor_venda':valor_venda})
+    #lista_produtos = conexao_bd.
     messagebox.showinfo("Sucesso", "Produto cadastrado com sucesso!")
 
 # Função para cadastrar clientes
 def cadastrar_cliente():
-    nome = entry_cliente.get()
-    telefone = entry_fone.get()
-
-    #cursor.execute("INSERT INTO clientes (nome, telefone) VALUES (%s, %s)", (nome, telefone))
-    #conn.commit()
+    print(f'nome {cliente}, telefone {telefone}')
+    conexao.cadastrar_cliente({'nome':cliente, 'telefone':telefone})
+    #lista_produtos = conexao_bd.
     messagebox.showinfo("Sucesso", "Cliente cadastrado com sucesso!")
 
 # Função para cadastrar vendas
@@ -76,10 +70,10 @@ tamanho.grid(row=1, column=0)
 entry_tamanho = Entry(interface)
 entry_tamanho.grid(row=1, column=1)
 
-valor = Label(interface, text="Valor Venda:")
-valor.grid(row=2, column=0)
-entry_valor = Entry(interface)
-entry_valor.grid(row=2, column=1)
+valor_venda = Label(interface, text="Valor Venda:")
+valor_venda.grid(row=2, column=0)
+entry_valor_venda = Entry(interface)
+entry_valor_venda.grid(row=2, column=1)
 
 btn_produto = Button(interface, text="Cadastrar Produto", command=cadastrar_produto)
 btn_produto.grid(row=3, column=1)
@@ -90,10 +84,10 @@ cliente.grid(row=6, column=0)
 entry_cliente = Entry(interface)
 entry_cliente.grid(row=6, column=1)
 
-fone = Label(interface, text="Telefone:")
-fone.grid(row=7, column=0)
-entry_fone = Entry(interface)
-entry_fone.grid(row=7, column=1)
+telefone = Label(interface, text="Telefone:")
+telefone.grid(row=7, column=0)
+entry_telefone = Entry(interface)
+entry_telefone.grid(row=7, column=1)
 
 btn_cliente = Button(interface, text="Cadastrar Cliente", command=cadastrar_cliente)
 btn_cliente.grid(row=8, column=1)
