80d3ea6f77ed1c4b0a3d510e17478af1bc154b2b
Desyree Garcia <desyree.garcia@hotmail.com>
2025-04-06 13:02:49 -0300

    interface produto e cliente

--- interface.py
+++ interface.py
@@ -8,14 +8,14 @@ from cliente import Cliente
 conexao = BandoDeDados("vendas_dgpijamas.db")
 
 # Função para cadastrar produtos
-def cadastrar_produto():
+def cadastrar_produto(categoria, tamanho, valor_venda):
     print(f'categoria {categoria}, tamanho {tamanho} e preço de venda {valor_venda}')
     conexao.cadastrar_produto({'categoria':categoria, 'tamanho':tamanho, 'valor_venda':valor_venda})
     #lista_produtos = conexao_bd.
     messagebox.showinfo("Sucesso", "Produto cadastrado com sucesso!")
 
 # Função para cadastrar clientes
-def cadastrar_cliente():
+def cadastrar_cliente(cliente, telefone):
     print(f'nome {cliente}, telefone {telefone}')
     conexao.cadastrar_cliente({'nome':cliente, 'telefone':telefone})
     #lista_produtos = conexao_bd.
@@ -60,37 +60,45 @@ interface = Tk()
 interface.title("DG Pijamas")
 
 # Formulário para cadastrar produtos
+titulo_cadastro_produtos = Label(interface, text="Cadastro de Produtos")
+titulo_cadastro_produtos.grid(row=1, column=0, columnspan=2, pady=10)
 categoria = Label(interface, text="Categoria:")
-categoria.grid(row=0, column=0)
+categoria.grid(row=2, column=0)
 entry_categoria = Entry(interface)
-entry_categoria.grid(row=0, column=1)
+entry_categoria.grid(row=2, column=1)
 
 tamanho = Label(interface, text="Tamanho:")
-tamanho.grid(row=1, column=0)
+tamanho.grid(row=3, column=0)
 entry_tamanho = Entry(interface)
-entry_tamanho.grid(row=1, column=1)
+entry_tamanho.grid(row=3, column=1)
 
 valor_venda = Label(interface, text="Valor Venda:")
-valor_venda.grid(row=2, column=0)
+valor_venda.grid(row=4, column=0)
 entry_valor_venda = Entry(interface)
-entry_valor_venda.grid(row=2, column=1)
+entry_valor_venda.grid(row=4, column=1)
 
-btn_produto = Button(interface, text="Cadastrar Produto", command=cadastrar_produto)
-btn_produto.grid(row=3, column=1)
+btn_produto = Button(
+    interface, text="Cadastrar Produto",
+    command=lambda:cadastrar_produto(entry_categoria.get(), entry_tamanho.get(), entry_valor_venda.get()))
+btn_produto.grid(row=5, column=1, pady=10)
 
 # Formulário para cadastrar clientes
+titulo_cadastro_cliente = Label(interface, text="Cadastro de Clientes")
+titulo_cadastro_cliente.grid(row=6, column=0, columnspan=2, pady=10)
 cliente = Label(interface, text="Nome:")
-cliente.grid(row=6, column=0)
+cliente.grid(row=7, column=0)
 entry_cliente = Entry(interface)
-entry_cliente.grid(row=6, column=1)
+entry_cliente.grid(row=7, column=1)
 
 telefone = Label(interface, text="Telefone:")
-telefone.grid(row=7, column=0)
+telefone.grid(row=8, column=0)
 entry_telefone = Entry(interface)
-entry_telefone.grid(row=7, column=1)
+entry_telefone.grid(row=8, column=1)
 
-btn_cliente = Button(interface, text="Cadastrar Cliente", command=cadastrar_cliente)
-btn_cliente.grid(row=8, column=1)
+btn_cliente = Button(
+    interface, text="Cadastrar Cliente",
+    command=lambda:cadastrar_cliente(entry_cliente.get(), entry_telefone.get()))
+btn_cliente.grid(row=9, column=1, pady=10)
 
 '''# Formulário para cadastrar vendas
 Label(interface, text="ID do Cliente:").pack()
